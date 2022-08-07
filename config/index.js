e1e09f53a764e3b8ecbe3162965c566d3c67a010
krishnanpandya007 <21dce067@charusat.edu.in>
2022-08-07 22:12:17 +0530

    Updated Backend URL

--- config/index.js
+++ config/index.js
@@ -22,5 +22,5 @@ export const docs_menu_options = [
   ]
 
 
-export const BACKEND_ROOT_URL = process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8000/' : 'http://143.244.140.90/'
+export const BACKEND_ROOT_URL = process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8000/' : 'http://159.65.152.43/'
 export const FRONTEND_ROOT_URL = process.env.NODE_ENV === "development" ? 'http://127.0.0.1:3000/' : 'https://docs.spadebeta.in/'
